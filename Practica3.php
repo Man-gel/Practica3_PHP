c69ead4b18162d7b27fb73332ceb6e25057b1094
Man-gel <night_captain@hotmail.com>
2015-02-25 22:28:06 -0600

    Se crean los arrays con la informacion de ls tweets

--- Practica3.php
+++ Practica3.php
@@ -1,58 +1,62 @@
 <!DOCTYPE html>
   <html>
 	<head>
-	  <title>Tablas</title>
+	  <title>Tweets</title>
 	</head>
 	<body>
 	  <h1>Alumnos</h1>
-	  <img src="https://www.google.es/images/srpr/logo11w.png" />
+	  <img src="https://www.google.es/images/srpr/logo11w.png" /><br/>
 		<?php
-		   $alumnos = array(
-		        'juan perez',
-			'jose Hernandez',
-			'Maria Fonseca',
-			1,
-			4.5
-		   );
-		   $alumnos[5] = 'Nuevo';
-		   $alumnos[]= 'Otro 1';
-		   $alumnos[] = 'Otro 2';
-		   echo '<h2>';
-		   echo $alumnos[7];
-		   echo '</h2>';
-		   $alumnos = array(
-		   /*0*/array(
-			'id' => '1',
-			'codigo' => '34354454',
-			'nombre' => 'Juan Perez'
-			),
+		   $tweets = array(
 		   /*1*/array(
-			'id' => '2',
-			'codigo' => '343344343',
-			'nombre' => 'Jose Hernandez'
-		   ),
+			'id' => '1',
+			'autor' => '@KurtKobain',
+			'contenido' => "Hey, did you see the amazing work made by Miguel?
+					It's really awesome!",
+			'fecha_hora' => 'Thu Feb 25th 2015 17:31:37',
+			'retweets' => '754'
+			),
 		   /*2*/array(
-			'id' => '3',
-			'codigo' => '7766776',
-			'nombre' => 'Maria Fonseca'
-		   ),
+			'id' => '2',
+			'autor' => '@KatyPerry',
+			'contenido' => "Oh yes! I just see it and I'm pretty glad 4 be so damn lucky!",
+			'fecha_hora' => 'Thu Feb 25th 2015 17:32:10',
+			'retweets' => '100'),
 		   /*3*/array(
+			'id' => '3',
+			'autor' => '@LinusTorvalds',
+			'contenido' => "I'm wondering if would he accept to work with me?",
+			'fecha_hora' => 'Thu Feb 25th 2015 17:32:58',
+			'retweets' => '1789'),
+		   /*4*/array(
 			'id' => '4',
-			'codigo' => '121211',
-			'nombre' => 'Margarita Herrera'
-		   ),
-		   array(
+			'autor' => '@Vaticano',
+			'contenido' => "Roguemos al Señor para que esa bendición que nos ha enviado
+					nos ilumine durante mucho tiempo.",
+			'fecha_hora' => 'Thu Feb 25th 2015 17:32:59',
+			'retweets' => '3000'),
+		   /*5*/array(
 			'id' => '5',
-			'codigo' => '3993546',
-			'nombre' => 'Jonathan'
-		   ),
-		   array(
+			'autor' => '@EPN',
+			'contenido' => "México manda una felicitación a la república de Italia por el gran talento que nos ha 
+					maravillado de su poeta Miguel Ángel.",
+			'fecha_hora' => 'Thu Feb 25th 2015 17:33:22',
+			'retweets' => '6510'),
+		   /*6*/array(
 			'id' => '6',
-			'codigo' => '1111',
-			'nombre' => 'Ana Valencia'
-		   )
+			'autor' => '@MiguelGirlfriendOfficial',
+			'contenido' => "Hey, all of you must be calm 'cause my boyfriend here, Miguel, don't read tweets!.ALVS 4ever4u!",
+			'fecha_hora' => 'Thu Feb 25th 2015 17:34:01',
+			'retweets' => '122'),
+		   /*7*/array(
+			'id' => '7',
+			'autor' => '@neo',
+			'contenido' => "Miguel, wake up. The Matrix has you.
+					would you take the red pill or the blue one?",
+			'fecha_hora' => 'Thu Feb 25th 2015 17:34:37',
+			'retweets' => '0')
 		   );
-		   echo '<label>'.$alumnos[2]['nombre'].'</label>';
+		   echo '<label>'.$tweets[2]['autor'].'</label>';
 		   echo '<br><br>';
 		?>
 		<br>
@@ -63,13 +67,15 @@
 		<th>C&oacute;digo</th>
 		<th>Nombre</th>
 		</tr>
-		<?php foreach ($alumnos as $alumno) { ?>
+		<?php /*foreach ($tweets as $tweet) { ?>
 		   <tr>
-		     <td><?php echo $alumno['id']; ?></td>
-		     <td><?php echo $alumno['codigo']; ?></td>
-		     <td><?php echo $alumno['nombre']; ?></td>
+		     <td><?php echo $tweet['id']; ?></td>
+		     <td><?php echo $tweet['autor']; ?></td>
+		     <td><?php echo $tweet['contenido']; ?></td>
+		     <td><?php echo $tweet['fecha_hora']; ?></td>
+		     <td><?php echo $tweet['retweets']; ?></td>
 		   </tr>
-		<?php } ?>
+		<?php } */?>
 	</table>
    </body>
 </html>
